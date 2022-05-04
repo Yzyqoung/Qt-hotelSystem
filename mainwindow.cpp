034b60da9a20c88d6198a0e832937b030df29011
杨涛 <578706463@qq.com>
2022-05-04 18:39:32 +0800

    优化mainwindow界面

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,21 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initwidget();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initwidget()
+{
+    fDP = new frmDbPage(this);
+    ui->stackedWidget->addWidget(fDP);
+}
+
+void MainWindow::on_btn_allorder_clicked()
+{
+    ui->stackedWidget->setCurrentWidget(fDP);
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,7 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <frmdbpage.h>
 namespace Ui {
 class MainWindow;
 }
@@ -14,9 +14,13 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void initwidget();
+private slots:
+    void on_btn_allorder_clicked();
 
 private:
     Ui::MainWindow *ui;
+    frmDbPage *fDP;
 };
 
 #endif // MAINWINDOW_H
